ad7571e3f3009275fd300c22267fcdd70fc04dae
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-04-25 17:03:50 +0200

    chore: Organize packages into subfolders

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,23 @@
 { pkgs, callPackage }:
-{
-  cryptpad = callPackage ./cryptpad {};
-  ferdium = callPackage ./ferdium {};
-  paru = callPackage ./paru {};
-}
+let
+  inherit (pkgs) lib;
+
+  importSub = prefix: attrs: lib.flip lib.mapAttrs attrs (
+    name: args: let
+      path = lib.path.append prefix name;
+    in callPackage path args
+  );
+
+  applications = importSub ./applications {
+    ferdium = { };
+    paru = { };
+  };
+
+  servers = importSub ./servers {
+    cryptpad = { };
+  };
+
+in lib.mergeAttrsList [
+  applications
+  servers
+]
