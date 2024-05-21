17880e460223abcd8231a6d3577c39e563ecbb2a
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-05-21 03:50:10 +0200

    freenginx: 1.26.0 -> 1.27.0

--- pkgs/servers/freenginx/default.nix
+++ pkgs/servers/freenginx/default.nix
@@ -23,11 +23,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "freenginx";
-  version = "1.26.0";
+  version = "1.27.0";
 
   src = fetchurl {
     url = "https://freenginx.org/download/freenginx-${version}.tar.gz";
-    hash = "sha256:1004krh2v5jc8zf0k7x7h4rlgblvjjll1vd5zdxi4mfnisprjbzb";
+    hash = "sha256-Z9toM50nCC/u/XAwa9eh5X7cK8PBaDL6mRypTp4AFk8=";
   };
 
   nativeBuildInputs = [
